9b1b2abd7984db3d83707d29a06ca0ff34f8e469
Val <valentin.belissen@limsi.fr>
2021-01-21 23:31:54 +0100

    op

--- scripts/video_to_openpose.sh
+++ scripts/video_to_openpose.sh
@@ -11,11 +11,11 @@ face=false
 
 mkdir "${path2features}${vidName}"
 if [[ "$hand" = true && "$face" = true ]]; then
-    ".${path2openPose}build/examples/openpose/openpose.bin" --video "${path2vid}${vidName}.${vidExt}" --write_keypoint_json "${path2features}${vidName}" --hand --hand_scale_number 3 --hand_scale_range 0.4 --face --no_display
+    "${path2openPose}build/examples/openpose/openpose.bin" --video "${path2vid}${vidName}.${vidExt}" --write_keypoint_json "${path2features}${vidName}" --hand --hand_scale_number 3 --hand_scale_range 0.4 --face --no_display
 elif [ "$hand" = true ]; then
-    ".${path2openPose}build/examples/openpose/openpose.bin" --video "${path2vid}${vidName}.${vidExt}" --write_keypoint_json "${path2features}${vidName}" --hand --hand_scale_number 3 --hand_scale_range 0.4 --no_display
+    "${path2openPose}build/examples/openpose/openpose.bin" --video "${path2vid}${vidName}.${vidExt}" --write_keypoint_json "${path2features}${vidName}" --hand --hand_scale_number 3 --hand_scale_range 0.4 --no_display
 elif [ "$face" = true ]; then
-    ".${path2openPose}build/examples/openpose/openpose.bin" --video "${path2vid}${vidName}.${vidExt}" --write_keypoint_json "${path2features}${vidName}" --face --no_display
+    "${path2openPose}build/examples/openpose/openpose.bin" --video "${path2vid}${vidName}.${vidExt}" --write_keypoint_json "${path2features}${vidName}" --face --no_display
 else
-    ".${path2openPose}build/examples/openpose/openpose.bin" --video "${path2vid}${vidName}.${vidExt}" --write_keypoint_json "${path2features}${vidName}" --no_display
+    "${path2openPose}build/examples/openpose/openpose.bin" --video "${path2vid}${vidName}.${vidExt}" --write_keypoint_json "${path2features}${vidName}" --no_display
 fi
