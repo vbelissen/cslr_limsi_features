3f472bab312ac3cea7981d520d3a39d1d1b5b1ba
Val <valentin.belissen@limsi.fr>
2021-01-25 13:59:34 +0100

    test

--- scripts/frames_to_3DFace.sh
+++ scripts/frames_to_3DFace.sh
@@ -7,13 +7,15 @@ function usage() {
   if [ -n "$1" ]; then
     echo -e "${RED}👉 $1${CLEAR}\n";
   fi
-  echo "Usage: $0 [-v vidName] [--vidExt]"
+  echo "Usage: $0 [-v vidName] [--vidExt] [--framesExt] [-n nDigits]]"
   echo "  -v, --vidName            Video name without extension"
   echo "  --vidExt                 Video file extension"
+  echo "  --framesExt              Frame files extension"
+  echo "  -n, --nDigits            Number of digits for frame numbering"
 #  echo "  --keep_full_frames       For not deleting full frames         (optional, default=0)"
 #  echo "  --keep_hand_crop_frames  For not deleting hand crop frames    (optional, default=0)"
   echo ""
-  echo "Example: $0 -v test_video_1 --vidExt mp4"
+  echo "Example: $0 -v test_video_1 --vidExt mp4 --framesExt jpg -n 5"
   exit 1
 }
 
@@ -21,12 +23,16 @@ function usage() {
 while [[ "$#" > 0 ]]; do case $1 in
   -v|--vidName) VIDNAME="$2"; shift;shift;;
   --vidExt) VIDEXT="$2"; shift;shift;;
+  --framesExt) FRAMESEXT="$2"; shift;shift;;
+  -n|--nDigits) NDIGITS="$2"; shift;shift;;
   *) usage "Unknown parameter passed: $1"; shift; shift;;
 esac; done
 
 # verify params
 if [ -z "$VIDNAME" ]; then usage "Video name is not set"; fi;
 if [ -z "$VIDEXT" ]; then usage "Video extension is not set."; fi;
+if [ -z "$FRAMESEXT" ]; then usage "Frames extension is not set."; fi;
+if [ -z "$NDIGITS" ]; then usage "Number of digits for frame numbering is not set."; fi;
 
 path2vid=`cat scripts/paths/path_to_videos.txt`
 path2features=`cat scripts/paths/path_to_features.txt`
@@ -38,5 +44,5 @@ vEnv=`cat scripts/virtual_env_names/vEnv_for_3DFace.txt`
 nImg=$(ls "${path2frames}${VIDNAME}/" | wc -l)
 
 source activate ${vEnv}
-python "${path2utils}frames_to_3DFace.py" ${nImg}
+python "${path2utils}frames_to_3DFace.py" ${nImg} ${path2frames} ${VIDNAME} ${FRAMESEXT} ${NDIGITS} ${path2features}
 source deactivate
