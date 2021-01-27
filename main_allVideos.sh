27a8b2d264948f48d8a42afb8a1024debc60e8ed
Valentin Belissen <valentin.belissen@limsi.fr>
2021-01-27 12:35:25 +0100

    main script

--- main_allVideos.sh
+++ main_allVideos.sh
@@ -7,9 +7,7 @@ function usage() {
   if [ -n "$1" ]; then
     echo -e "${RED}👉 $1${CLEAR}\n";
   fi
-  echo "Usage: $0 [-v vidName] [--vidExt] [--framesExt] [-n nDigits] [--handOP] [--faceOP] [--body3D] [--face3D] [--hs] [--keep_full_frames] [--keep_hand_crop_frames]"
-  echo "  -v, --vidName            Video name without extension"
-  echo "  --vidExt                 Video file extension"
+  echo "Usage: $0 [--framesExt] [-n nDigits] [--handOP] [--faceOP] [--body3D] [--face3D] [--hs] [--keep_full_frames] [--keep_hand_crop_frames]"
   echo "  --framesExt              Frame files extension"
   echo "  -n, --nDigits            Number of digits for frame numbering"
   echo "  --handOP                 OpenPose computed on hands too"
@@ -20,7 +18,7 @@ function usage() {
   echo "  --keep_full_frames       For not deleting full frames         (optional, default=0)"
   echo "  --keep_hand_crop_frames  For not deleting hand crop frames    (optional, default=0)"
   echo ""
-  echo "Example: $0 -v test_video_1 --vidExt mp4 --framesExt jpg -n 5 --handOP --faceOP --body3D --face3D --hs --keep_full_frames --keep_hand_crop_frames"
+  echo "Example: $0 --framesExt jpg -n 5 --handOP --faceOP --body3D --face3D --hs --keep_full_frames --keep_hand_crop_frames"
   exit 1
 }
 
@@ -37,17 +35,21 @@ HANDOP_STRING=""
 FACEOP_STRING=""
 BODY3D_STRING=""
 FACE3D_STRING=""
+HS_STRING=""
+KEEP_FULL_FRAMES_STRING=""
+KEEP_HAND_CROP_FRAMES_STRING=""
 
 if [[ "$HANDOP" = true ]]; then HANDOP_STRING=" --handOP"; fi;
 if [[ "$FACEOP" = true ]]; then FACEOP_STRING=" --faceOP"; fi;
 if [[ "$BODY3D" = true ]]; then BODY3D_STRING=" --body3D"; fi;
 if [[ "$FACE3D" = true ]]; then FACE3D_STRING=" --face3D"; fi;
+if [[ "$HS" = true ]]; then HS_STRING=" --hs"; fi;
+if [[ "$KEEP_FULL_FRAMES" = true ]]; then KEEP_FULL_FRAMES_STRING=" --keep_full_frames"; fi;
+if [[ "$KEEP_HAND_CROP_FRAMES" = true ]]; then KEEP_HAND_CROP_FRAMES_STRING=" --keep_hand_crop_frames"; fi;
 
 
 # parse params
 while [[ "$#" > 0 ]]; do case $1 in
-  -v|--vidName) VIDNAME="$2"; shift;shift;;
-  --vidExt) VIDEXT="$2"; shift;shift;;
   --framesExt) FRAMESEXT="$2"; shift;shift;;
   -n|--nDigits) NDIGITS="$2"; shift;shift;;
   --handOP) HANDOP=true; shift;;
@@ -61,20 +63,16 @@ while [[ "$#" > 0 ]]; do case $1 in
 esac; done
 
 # verify params
-if [ -z "$VIDNAME" ]; then usage "Video name is not set"; fi;
-if [ -z "$VIDEXT" ]; then usage "Video extension is not set."; fi;
 if [ -z "$FRAMESEXT" ]; then usage "Frames extension is not set."; fi;
 if [ -z "$NDIGITS" ]; then usage "Number of digits for frame numbering is not set."; fi;
 
-path2frames=`cat scripts/paths/path_to_frames.txt`
-path2handFrames=`cat scripts/paths/path_to_hand_frames.txt`
-
-video_to_frames.sh -v ${VIDNAME} --vidExt ${VIDEXT} --framesExt ${FRAMESEXT} -n ${NDIGITS}
-video_to_openpose.sh -v ${VIDNAME} --vidExt ${VIDEXT} ${HANDOP_STRING}${FACEOP_STRING}
-openpose_json_to_clean_data.sh -v ${VIDNAME} ${HANDOP_STRING}${FACEOP_STRING}
-openpose_clean_to_hand_crops.sh -v ${VIDNAME} --vidExt ${VIDEXT} --framesExt ${FRAMESEXT} -n ${NDIGITS}
-openpose_clean_to_2D_3D.sh -v ${VIDNAME} ${HANDOP_STRING}${FACEOP_STRING}${BODY3D}${FACE3D}
-if [[ "$HS" = true ]]; then hand_crops_to_HS_probabilities.sh -v ${VIDNAME} -n ${NDIGITS}; fi;
-
-if [[ "$KEEP_FULL_FRAMES" = false ]]; then rm -rf ${path2frames}${VIDNAME}; fi;
-if [[ "$KEEP_HAND_CROP_FRAMES" = false ]]; then rm -rf ${path2handFrames}${VIDNAME}; fi;
+path2vid=`cat scripts/paths/path_to_videos.txt`
+yourfilenames=`ls ${path2vid}`
+for file in ${yourfilenames}; do
+    filename=$(basename -- "$file")
+    extension="${filename##*.}"
+    filename="${filename%.*}"
+    main_uniqueVideo.sh -v ${filename} --vidExt ${extension} --framesExt ${FRAMESEXT} -n ${NDIGITS} ${HANDOP_STRING}${FACEOP_STRING}${BODY3D_STRING}${FACE3D_STRING}${HS_STRING}${KEEP_FULL_FRAMES_STRING}${KEEP_HAND_CROP_FRAMES_STRING}
+    echo $filename
+    echo $extension
+done
