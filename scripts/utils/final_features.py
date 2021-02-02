96b8da2372d53e508760e15115a10223ef2191de
Valentin Belissen <valentin.belissen@limsi.fr>
2021-02-02 10:27:20 +0100

    main script

--- scripts/utils/final_features.py
+++ scripts/utils/final_features.py
@@ -18,11 +18,10 @@ with open(path2leftHandedList, 'r', encoding='utf-8-sig') as open_left_handed:
     reader_left_handed = csv.reader(open_left_handed, delimiter=';')
     for i in reader_left_handed:
         video_list.append(i[0])
-        left_handed_list.append(i[1])
+        left_handed_list.append(int(i[1]))
 
 
 left_handed_found = find_element_in_list(vidName, video_list)
-print(left_handed_list)
 if left_handed_found is None:
     left_handed = False
 else:
