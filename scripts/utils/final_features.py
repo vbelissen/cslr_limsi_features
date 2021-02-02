3520946209ac8d9726995dbc5c72f90b52b781ce
Valentin Belissen <valentin.belissen@limsi.fr>
2021-02-02 10:20:08 +0100

    main script

--- scripts/utils/final_features.py
+++ scripts/utils/final_features.py
@@ -2,18 +2,27 @@ import numpy as np
 import csv
 
 
+vidName             = 'DictaSign_lsf_S8_T2_B4_front'
 path2leftHandedList = '/people/belissen/Python/CSLR_LIMSI/cslr_limsi_features/left_handed/left_handed_dictasign.csv'
+
+def find_element_in_list(element, list_element):
+    try:
+        index_element = list_element.index(element)
+        return index_element
+    except ValueError:
+        return None
 
 video_list = []
 left_handed_list = []
 with open(path2leftHandedList, 'r', encoding='utf-8-sig') as open_left_handed:
     reader_left_handed = csv.reader(open_left_handed, delimiter=';')
     for i in reader_left_handed:
-        print(i)
-        video_list.append(i[0])
-        left_handed_list.append(i[1])
 
-#gaucher =
+left_handed_found = find_element_in_list(vidName, video_list)
+if left_handed_found is None:
+    left_handed = False
+else:
+    left_handed = bool(left_handed_list[left_handed_found])
 
-print(video_list)
-print(left_handed_list)
+
+print(left_handed)
